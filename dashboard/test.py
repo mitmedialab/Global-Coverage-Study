d7a29b5b4cf730358647d45f23cf0e8129d8f608
Rahul Bhargava <rahulb@media.mit.edu>
2014-03-06 14:22:04 -0500

    renamed media module to dashboard for clarity (was confusing cause we have a mediacloud.media module too!)

--- dashboard/test.py
+++ dashboard/test.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from media.test.sourcetest import MediaSourceCollectionTest
+from dashboard.test.sourcetest import MediaSourceCollectionTest
 
 suite = unittest.TestLoader().loadTestsFromTestCase(MediaSourceCollectionTest)
 unittest.TextTestRunner(verbosity=2).run(suite)
