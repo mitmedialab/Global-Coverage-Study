40c5f89d721ab0aa3ea08c83cef836fb6065a851
Rahul Bhargava <rahulb@media.mit.edu>
2014-04-08 12:22:15 -0400

    better logging

--- story-fetcher/fetch-stories.py
+++ story-fetcher/fetch-stories.py
@@ -7,7 +7,7 @@ STORIES_PER_PAGE = 1000
 
 logging.basicConfig(filename='fetcher.log',level=logging.DEBUG)
 log = logging.getLogger('fetcher')
-log.info("---------------------------------------------------------------------------")
+log.info("======================================================================")
 
 collection = mediameter.source.MediaSourceCollection()
 collection.loadAllMediaIds()
@@ -18,6 +18,7 @@ log.info('Loaded '+str(collection.count())+' media sources to pull')
 
 # walk through all the sources, grabbing all the stories from each
 for source in collection.mediaSources():
+    log.info("---------------------------------------------------------------------------")
     log.info('  Starting with '+source['url']+' ('+source['media_id']+'): '+source['category'])
     
     query_str = '*'
@@ -30,7 +31,11 @@ for source in collection.mediaSources():
     while more_stories:
         log.info('    loading stories from '+str(last_processed_stories_id))
         stories = mc.storyList(query_str, filter_str, last_processed_stories_id, STORIES_PER_PAGE)
-        for story in stories:
-            db.addStory(story, {'type':source['category']})
-        last_processed_stories_id = stories[len(stories)-1]['stories_id']
-        more_stories = True if len(stories)>0 else False
+        if len(stories)>0:
+            for story in stories:
+                db.addStory(story, {'type':source['category']})
+            last_processed_stories_id = stories[len(stories)-1]['stories_id']
+            more_stories = True
+        else:
+            more_stories = False
+    log.info('  Done with '+source['url']+' ('+source['media_id']+'): '+source['category'])
