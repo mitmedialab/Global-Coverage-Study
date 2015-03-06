335a9044039930a6dd3b51031ed58deffa007c40
Rahul Bhargava <rahulb@media.mit.edu>
2015-03-06 12:17:21 -0500

    updated to CLIFF v1.2.0

--- media-source-dashboard/mediameter/cliff.py
+++ media-source-dashboard/mediameter/cliff.py
@@ -6,9 +6,8 @@ class Cliff():
     Make requests to a CLIFF geo-parsing / NER server
     '''
 
-    PARSE_TEXT_PATH = "/CLIFF-1.1.1/parse/text"
-    PARSE_NLP_JSON_PATH = "/CLIFF-1.1.1/parse/json"
-    GEONAMES_LOOKUP_PATH = "/CLIFF-1.1.1/geonames"
+    PARSE_TEXT_PATH = "/CLIFF-1.2.0/parse/text"
+    PARSE_NLP_JSON_PATH = "/CLIFF-1.2.0/parse/json"
 
     JSON_PATH_TO_ABOUT_COUNTRIES = 'results.places.about.countries';
 
@@ -21,13 +20,10 @@ class Cliff():
         self._log.info("CLIFF @ %s:%d", self._host,self._port)
 
     def parseText(self,text,demonyms=False):
-        return self._parseQuery(self.PARSE_TEXT_PATH, text, demonyms)
+        return self._query(self.PARSE_TEXT_PATH, text, demonyms)
 
     def parseNlpJson(self,json_object,demonyms=False):
-        return self._parseQuery(self.PARSE_NLP_JSON_PATH, json.dumps(json_object), demonyms)
-
-    def geonamesLookup(self,geonames_id):
-        return self._query(self.GEONAMES_LOOKUP_PATH, {'id':geonames_id})['results']
+        return self._query(self.PARSE_NLP_JSON_PATH, json.dumps(json_object), demonyms)
 
     def _demonymsText(self, demonyms=False):
         return "true" if demonyms else "false"
@@ -35,16 +31,14 @@ class Cliff():
     def _urlTo(self, path):
         return self._host+":"+str(self._port)+path
 
-    def _parseQuery(self,path,text,demonyms=False):
+    def _query(self,path,text,demonyms=False):
         payload = {'q':text,'replaceAllDemonyms':self._demonymsText(demonyms)}
         self._log.debug("Querying "+path+" (demonyms="+str(demonyms)+")")
-        return self._query(path,payload)
-    
-    def _query(self,path,args):
         try:
-            r = requests.post( self._urlTo(path), data=args)
-            self._log.debug('CLIFF says '+r.content)
+            r = requests.post( self._urlTo(path), data=payload)
+            #self._log.debug('CLIFF says '+r.content)
             return r.json()
         except requests.exceptions.RequestException as e:
             self._log.error("RequestException " + str(e))
         return ""
+
